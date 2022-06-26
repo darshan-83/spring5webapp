6416ea6abf3fe859250800ef4a1e75b5e3b9ab79
Darshan <darshankheni83@gmail.com>
2022-06-26 15:23:07 +0530

    aa

---
+++ src/main/java/com/spring/spring5webapp/spring5weapp/Spring5weappApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring5webapp.spring5weapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5weappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5weappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/spring5webapp/spring5weapp/Spring5weappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring5webapp.spring5weapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5weappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
